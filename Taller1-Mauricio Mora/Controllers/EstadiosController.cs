2caec81cc412d979d14c8a2447b1bb5eb8571900
Mauro0407 <mauricio.mora.caicedo@udla.edu.ec>
2024-10-24 09:14:35 -0500

    5to Commit:

--- Taller1-Mauricio Mora/Controllers/EstadiosController.cs
+++ Taller1-Mauricio Mora/Controllers/EstadiosController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.Rendering;//hola
 using Microsoft.EntityFrameworkCore;
 using Taller1_Mauricio_Mora.Data;
 using Taller1_Mauricio_Mora.Models;
